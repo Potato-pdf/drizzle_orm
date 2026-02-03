0352b32fd99824336621db9d30fb7ab5e1b63fea
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-02-03 13:18:32 -0600

    feat: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "dotenv/config"
 import { drizzle } from "drizzle-orm/node-postgres"
+import { UserInsert } from "./db/schema"
+
 const DB_URL = process.env.DATABASE_URL;
 
 if (!DB_URL) {
@@ -7,3 +9,12 @@ if (!DB_URL) {
 }
 const db = drizzle(DB_URL);
 
+async function main() {
+    const newUSer: UserInsert = {
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+})
