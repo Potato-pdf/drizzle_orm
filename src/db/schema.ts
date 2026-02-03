c54aefc20fd7863a62c91d192834198ffa09678c
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-02-03 12:52:31 -0600

    feat: drizzle config

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom().unique(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    age: integer("age").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+});
