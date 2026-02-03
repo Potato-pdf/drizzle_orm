99bd5ff1473393d4e1dab0ecf20437711f32d1a2
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-02-03 13:15:31 -0600

    feat: igrations

---
+++ src/db/migrations/0000_watery_paibok.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"age" integer NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_id_unique" UNIQUE("id"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+
+export type UserInsert = typeof users.$inferInsert;
